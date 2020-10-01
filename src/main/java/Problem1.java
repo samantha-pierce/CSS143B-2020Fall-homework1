bae5cb2e586b6424a265eec560bd148bf8518763
Samantha-Pierce <spapierce@gmail.com>
2020-10-01 12:28:19 -0700

    added a base case

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,6 +3,9 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
+        if (data == null) {
+            return -1;
+        }
         return -1; // Place holder
     }
 }
