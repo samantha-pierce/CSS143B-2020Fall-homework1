6333247c6b1c61ac140336480a1eae2f7ccbcdea
Samantha-Pierce <spapierce@gmail.com>
2020-10-01 12:37:48 -0700

    added another base case when input value equals target

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,8 +3,12 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        if (data == null) {
+        int index = 0;
+        if (data.length == 0) {
             return -1;
+        }
+        if (data[index] == target) {
+            return index;
         }
         return -1; // Place holder
     }
