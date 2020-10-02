413ed2c83099882d2a9959b2ed672ac52f2674ec
Samantha-Pierce <spapierce@gmail.com>
2020-10-02 09:12:13 -0700

    added a variable for the middle index

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,12 +3,15 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        int index = 0;
+        // finding the middle index
+        int midIndex = (data.length - 1) / 2;
         if (data.length == 0) {
             return -1;
         }
-        if (data[index] == target) {
-            return index;
+        if (data.length == 1) {
+            if (data[midIndex] == target) {
+                return midIndex;
+            }
         }
         return -1; // Place holder
     }
