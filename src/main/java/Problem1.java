95ec8243604577a38a66e3ec84d7cb233777accd
Samantha-Pierce <spapierce@gmail.com>
2020-10-03 10:39:12 -0700

    passes given tests

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,16 +3,30 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        // finding the middle index
-        int midIndex = (data.length - 1) / 2;
         if (data.length == 0) {
             return -1;
         }
-        if (data.length == 1) {
-            if (data[midIndex] == target) {
-                return midIndex;
-            }
-        }
-        return -1; // Place holder
+        return recursiveSearch(data, 0, data.length - 1, target);
+        //return index;
+        //return -1; // Place holder
     }
+
+    //helper method
+    private static int recursiveSearch(int[] data, int start, int end, int target) {
+        int mid = (start + end) / 2;
+        if (start > end) {
+            return -1;
+        }
+        if (data[mid] == target) {
+            return mid;
+        // check left half
+        } else if (target < data[mid]) {
+            return recursiveSearch(data, start, mid - 1, target);
+        } //else if (target > data[mid]) {
+            // check right half
+        return recursiveSearch(data, mid + 1, end, target);
+    }
+
+    // credit to https://javarevisited.blogspot.com/2017/04/recursive-binary-search-algorithm-in-java-example.html
+    // for help with writing helper method
 }
