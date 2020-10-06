707816b70b5461ae214f496ac98b706603f35257
Samantha-Pierce <spapierce@gmail.com>
2020-10-06 09:54:24 -0700

    implemented bubble sort and tests

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class Problem2Test {
@@ -8,7 +10,30 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
+        // assertTrue(false);
         // add your tests here
+        int inputs[][] = {
+                {},
+                {3},
+                {2,1},
+                {8,2,4},
+                {20,8,10,5,1},
+        };
+
+        int answer[][] = {
+                {},
+                {3},
+                {1,2},
+                {2,4,8},
+                {1,5,8,10,20},
+        };
+
+        assertEquals(inputs.length, answer.length);
+
+        for (int i=0; i<inputs.length; i++) {
+            Problem2.bubbleSort(inputs[i]);
+            assertEquals(Arrays.toString(answer[i]), Arrays.toString(inputs[i]));
+        }
     }
+
 }
